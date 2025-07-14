3551ec64296df940783cb3f6b4e1192157f7f4cc
cudema <54349175+cudema@users.noreply.github.com>
2025-07-14 16:30:54 +0900

    베어가르기 추가
    베어가르기 패턴 추가

---
+++ Assets/Script/Enemy/Pattern/Bullet/CutBullet.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CutBullet : BulletBase
+{
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Enemy/Pattern/EB_2/Cut.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cut", menuName = "Scriptable Objects/Cut")]
+public class Cut : PatternBase
+{
+    public override void StartPattern()
+    {
+        base.StartPattern();
+    }
+
+    protected override IEnumerator BingPattern()
+    {
+        yield return new WaitForSeconds(attackDelay);
+
+        Vector3 temp = new Vector3(PlayerContoller.instance.transform.position.x, BattleManager.battlemanager.Center.y, enemy.transform.position.z);
+
+        for (int i = 0; i < bulletCount; i++)
+        {
+            yield return new WaitForSeconds(bulletSpawnDelay);
+            go = Instantiate(bullet, temp, Quaternion.identity, bulletParent);
+            go.GetComponent<BulletBase>().Setup(this);
+            Destroy(go, 1f);
+        }
+
+        yield return new WaitUntil(() => go == null);
+
+        BattleManager.ChangeTrun(Trun.¾Æ±º);
+    }
+}

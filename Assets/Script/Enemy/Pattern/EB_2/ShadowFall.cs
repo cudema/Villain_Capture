7f2a27699414b4f1f1de0099f8c9000dc375b2d8
cudema <54349175+cudema@users.noreply.github.com>
2025-07-15 14:06:02 +0900

    그림자 낙하 추가
    그림자 낙하 패턴 추가

---
+++ Assets/Script/Enemy/Pattern/EB_2/ShadowFall.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShadowFall", menuName = "Scriptable Objects/ShadowFall")]
+public class ShadowFall : PatternBase
+{
+    [Header("Âï±â")]
+    [SerializeField]
+    float enemySpawnDelay;
+    [SerializeField]
+    float rushDelay;
+    [SerializeField]
+    float rushSpeed;
+    [SerializeField]
+    float rushDamage;
+    [SerializeField]
+    int rushCount;
+
+    public override void StartPattern()
+    {
+        base.StartPattern();
+    }
+
+    protected override IEnumerator BingPattern()
+    {
+        float floorY = BattleManager.battlemanager.Center.y - BattleManager.battlemanager.Radius.y;
+        float spawnY = BattleManager.battlemanager.Center.y + BattleManager.battlemanager.Radius.y + 2;
+
+        for (int i = 0; i < rushCount; i++)
+        {
+            enemy.OffRenderer();
+
+            yield return new WaitForSeconds(enemySpawnDelay);
+
+            enemy.transform.position = new Vector3(PlayerContoller.instance.transform.position.x, spawnY, enemy.transform.position.z);
+            enemy.OnRenderer();
+
+            yield return new WaitForSeconds(rushDelay);
+
+            while (enemy.transform.position.y > floorY)
+            {
+                enemy.transform.position += Vector3.down * rushSpeed * Time.deltaTime;
+                yield return null;
+            }
+            go = Instantiate(bullet, enemy.transform.position + Vector3.left * 0.7f, Quaternion.identity, bulletParent);
+            go.GetComponent<BulletBase>().Setup(this);
+            go = Instantiate(bullet, enemy.transform.position + Vector3.right * 0.7f, Quaternion.Euler(new Vector3(0, 0, 180)), bulletParent);
+            go.GetComponent<BulletBase>().Setup(this);
+
+            yield return new WaitForSeconds(bulletSpawnDelay);
+        }
+
+        yield return new WaitForSeconds(bulletSpawnDelay);
+
+        StopPattern();
+    }
+}
